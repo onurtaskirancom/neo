0370cee443b92d8de5e163142fbeb9e6fda014db
onurtaskirancom <taskiranonur9@gmail.com>
2022-03-21 22:29:51 +0300

    Signup route and controller setup

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 require("dotenv").config();
 // bring routes
-const blogRoutes = require('./routes/blog')
+const blogRoutes = require("./routes/blog");
+const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
@@ -26,12 +27,11 @@ if (process.env.NODE_ENV === "development") {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 // routes middleware
-app.use('/api', blogRoutes);
+app.use("/api", blogRoutes);
+app.use("/api", authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
