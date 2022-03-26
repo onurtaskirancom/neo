25e80b6fe56b9c6c6301433899bcf3dd9bae3539
onurtaskirancom <taskiranonur9@gmail.com>
2022-03-26 17:25:51 +0300

    Signout and protected routes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,8 @@
 const User = require("../models/user");
 const shortId = require("shortid");
 const user = require("../models/user");
-const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt')
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
   User.findOne({ email: req.body.email }).exec((err, user) => {
@@ -43,19 +43,34 @@ exports.signin = (req, res) => {
       });
     }
     // authenticate
-    if(!user.authenticate(password)) {
+    if (!user.authenticate(password)) {
       return res.status(400).json({
         error: "Email and password do not match.",
       });
     }
     // generate a token and send to client
-    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
-    res.cookie('token', token, {expiresIn: '1d'})
+    res.cookie("token", token, { expiresIn: "1d" });
     const { _id, username, name, email, role } = user;
     return res.json({
       token,
-      user: {_id, username, name, email, role }
+      user: { _id, username, name, email, role },
     });
   });
 };
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "Signout success",
+  });
+};
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"], // added later
+  userProperty: "auth",
+});
