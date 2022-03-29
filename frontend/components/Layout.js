52add8faf502623b11c1b9444081a2edcacf699a
onurtaskirancom <taskiranonur9@gmail.com>
2022-03-29 03:52:16 +0300

    frontend signin signup

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from 'react'
+import Header from './Header'
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
-      <p>header</p>
+      <Header />
       {children}
-      <p>footer</p>
     </React.Fragment>
   );
 };
