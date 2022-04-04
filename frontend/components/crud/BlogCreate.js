47b6ed8bd4c5118eb8f5b0702fbef79ec1558f58
onurtaskirancom <taskiranonur9@gmail.com>
2022-04-04 21:33:42 +0300

    Toggle categories check uncheck and Publish blog

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -4,7 +4,7 @@ import Router from "next/router";
 import dynamic from "next/dynamic";
 import { withRouter } from "next/router";
 import { getCookie, isAuth } from "../../actions/auth";
-import { getCategories } from "../../actions/category";
+import { create, getCategories } from "../../actions/category";
 import { getTags } from "../../actions/tag";
 import { createBlog } from "../../actions/blog";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
@@ -26,6 +26,9 @@ const CreateBlog = ({ router }) => {
   const [categories, setCategories] = useState([]);
   const [tags, setTags] = useState([]);
 
+  const [checked, setChecked] = useState([]); // categories
+  const [checkedTag, setCheckedTag] = useState([]); // tags
+
   const [body, setBody] = useState(blogFromLS());
   const [values, setValues] = useState({
     error: "",
@@ -38,6 +41,7 @@ const CreateBlog = ({ router }) => {
 
   const { error, sizeError, success, formData, title, hidePublishButton } =
     values;
+  const token = getCookie("token");
 
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
@@ -67,7 +71,22 @@ const CreateBlog = ({ router }) => {
 
   const publishBlog = (e) => {
     e.preventDefault();
-    console.log("ready to publishBlog");
+    // console.log("ready to publishBlog");
+    createBlog(formData, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          title: "",
+          error: "",
+          success: `A new blog titled "${data.title}" is created`,
+        });
+        setBody("");
+        setCategories([]);
+        setTags([]);
+      }
+    });
   };
 
   const handleChange = (name) => (e) => {
@@ -86,27 +105,71 @@ const CreateBlog = ({ router }) => {
     }
   };
 
+  const handleToggle = (c) => () => {
+    setValues({ ...values, error: "" });
+
+    // return the first index or -1
+    const clickedCategory = checked.indexOf(c);
+    const all = [...checked];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log(all);
+    setChecked(all);
+    formData.set("categories", all);
+  };
+
+  const handleTagsToggle = (t) => () => {
+    setValues({ ...values, error: "" });
+
+    // return the first index or -1
+    const clickedTag = checked.indexOf(t);
+    const all = [...checkedTag];
+
+    if (clickedTag === -1) {
+      all.push(t);
+    } else {
+      all.splice(clickedTag, 1);
+    }
+    console.log(all);
+    setCheckedTag(all);
+    formData.set("tags", all);
+  };
+
   const showCategories = () => {
     return (
-      categories && categories.map((c, i) => (
-        <li key={i} className="list-unstyled">
-          <input type="checkbox" className="mr-2" />
-          <label className="form-check-label">{c.name}</label>
+      categories &&
+      categories.map((c, i) => (
+        <li key={i} className='list-unstyled'>
+          <input
+            onChange={handleToggle(c._id)}
+            type='checkbox'
+            className='mr-2'
+          />
+          <label className='form-check-label'>{c.name}</label>
         </li>
       ))
-    )
-  }
+    );
+  };
 
   const showTags = () => {
     return (
-      tags && tags.map((t, i) => (
-        <li key={i} className="list-unstyled">
-          <input type="checkbox" className="mr-2" />
-          <label className="form-check-label">{t.name}</label>
+      tags &&
+      tags.map((t, i) => (
+        <li key={i} className='list-unstyled'>
+          <input
+            onChange={handleTagsToggle(t._id)}
+            type='checkbox'
+            className='mr-2'
+          />
+          <label className='form-check-label'>{t.name}</label>
         </li>
       ))
-    )
-  }
+    );
+  };
 
   const createBlogForm = () => {
     return (
@@ -154,16 +217,37 @@ const CreateBlog = ({ router }) => {
           <br />
           {JSON.stringify(tags)}
         </div>
-        <div className="col-md-4">
+        <div className='col-md-4'>
+          <div>
+            <div className='form-group pb-2'>
+              <h5>Featured image</h5>
+              <br />
+
+              <small className='text-muted'>Max size: 1mb</small>
+              <label className='btn btn-outline-info'>
+                Upload featured image
+                <input
+                  onChange={handleChange("photo")}
+                  type='file'
+                  accept='image/*'
+                  hidden
+                />
+              </label>
+            </div>
+          </div>
           <div>
             <h5>Categories</h5>
-            <br/>
-            <ul style={{maxHeight: '200px', overflowY: 'scroll'}}>{showCategories()}</ul>
+            <br />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
           </div>
           <div>
             <h5>Tags</h5>
-            <br/>
-            <ul style={{maxHeight: '200px', overflowY: 'scroll'}}>{showTags()}</ul>
+            <br />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showTags()}
+            </ul>
           </div>
         </div>
       </div>
