9ccab6f290e453bf548675f6fc5e0204f8d3c540
onurtaskirancom <taskiranonur9@gmail.com>
2022-04-04 18:31:42 +0300

    Quill modules, event handlers and localstorage

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -7,61 +7,128 @@ import { getCookie, isAuth } from "../../actions/auth";
 import { getCategories } from "../../actions/category";
 import { getTags } from "../../actions/tag";
 import { createBlog } from "../../actions/blog";
-const ReactQuill = dynamic(() => import('react-quill'), {ssr: false})
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "../../node_modules/react-quill/dist/quill.snow.css";
 
-const CreateBlog = ({router}) => {
+const CreateBlog = ({ router }) => {
+  const blogFromLS = () => {
+    if(typeof window === 'undefined') {
+      return false;
+    }
 
-  const [body, setBody] = useState({})
-  const [values, setValues] = useState({
-    error: '',
-    sizeError: '',
-    success: '',
-    formData: '',
-    title: '',
-    hidePublishButton: false
-  })
+    if(localStorage.getItem('blog')) {
+      return JSON.parse(localStorage.getItem('blog'));
+    } else {
+      return false;
+    }
 
-  const {error, sizeError, success, formData, title, hidePublishButton} = values;
-
-  const publishBlog = (e) => {
-    e.preventDefault()
-    console.log('ready to publishBlog')
   }
 
-  const handleChange = name => e => {
-    console.log(e.target.value); 
+
+  const [body, setBody] = useState(blogFromLS());
+  const [values, setValues] = useState({
+    error: "",
+    sizeError: "",
+    success: "",
+    formData: "",
+    title: "",
+    hidePublishButton: false,
+  });
+
+  const { error, sizeError, success, formData, title, hidePublishButton } = values;
+
+  useEffect(() => {
+    setValues({...values, formData: new FormData()})
+  }, [router])
+
+  const publishBlog = (e) => {
+    e.preventDefault();
+    console.log("ready to publishBlog");
   };
 
-  const handleBody = e => {
-    console.log(e); 
+  const handleChange = (name) => (e) => {
+    // console.log(e.target.value);
+    const value = name === 'photo' ? e.target.files[0] : e.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value, formData, error: '' });
+  };
+
+  const handleBody = (e) => {
+     // console.log(e);
+     setBody(e);
+     formData.set('body', e);
+     if (typeof window !== 'undefined') {
+         localStorage.setItem('blog', JSON.stringify(e));
+     }
   };
 
   const createBlogForm = () => {
     return (
       <form onSubmit={publishBlog}>
-        <div className="form-group">
-          <label className="text-muted">Title</label>
-          <input type="text" className="form-control" value={title} onChange={handleChange('title')} />
+        <div className='form-group'>
+          <label className='text-muted'>Title</label>
+          <input
+            type='text'
+            className='form-control'
+            value={title}
+            onChange={handleChange("title")}
+          />
         </div>
 
-        <div className="form-group">
-          <ReactQuill value={body} placeholder="Write something amazing..." onChange={handleBody}/>
+        <div className='form-group'>
+          <ReactQuill
+            modules={CreateBlog.modules}
+            formats={CreateBlog.formats}
+            value={body}
+            placeholder='Write something amazing...'
+            onChange={handleBody}
+          />
         </div>
 
         <div>
-          <button type="submit" className="btn btn-primary">
+          <button type='submit' className='btn btn-primary'>
             Publish
           </button>
         </div>
-
       </form>
-    )
-  }
+    );
+  };
 
-  return (
-    <div>{createBlogForm()}</div>
-  );
+  return <div>{createBlogForm()}
+  <br/>
+  {JSON.stringify(title)}
+  <br/>
+  {JSON.stringify(body)}
+  </div>;
 };
+
+CreateBlog.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "video"],
+    ["clean"],
+    ["code-block"],
+  ],
+};
+
+CreateBlog.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "link",
+  "image",
+  "video",
+  "code-block",
+];
 
 export default withRouter(CreateBlog);
