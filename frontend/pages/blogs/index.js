74bf6cdcd0564604f01be4efdd7669865fe54e85
onurtaskirancom <taskiranonur9@gmail.com>
2022-04-06 20:36:27 +0300

    Show all categories and tags

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -6,6 +6,7 @@ import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import Card from '../../components/blog/Card';
 
 const Blogs = ({ blogs, categories, tags, size }) => {
+
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
       // ()
@@ -17,6 +18,24 @@ const Blogs = ({ blogs, categories, tags, size }) => {
       );
     });
   };
+
+  const showAllCategories = () => {
+    return categories.map((c, i) => (
+      <Link href={`/categories/${c.slug}`} key={i}>
+        <a className="btn btn-primary mr-1 ml-1 mt-3">{c.name}</a>
+      </Link>
+    ))
+  }
+
+  const showAllTags = () => {
+    return tags.map((t, i) => (
+      <Link href={`/tags/${t.slug}`} key={i}>
+        <a className="btn btn-outline-primary mr-1 ml-1 mt-3">{t.name}</a>
+      </Link>
+    ))
+  }
+
+
   return (
     <Layout>
       <main>
@@ -28,7 +47,11 @@ const Blogs = ({ blogs, categories, tags, size }) => {
               </h1>
             </div>
             <section>
-              <p>show categories and tags</p>
+              <div className="pb-5 text-center">
+                {showAllCategories()}
+                <br />
+                {showAllTags()}
+              </div>
             </section>
           </header>
         </div>
