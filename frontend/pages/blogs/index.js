1cbe84befd865390283d3b20f0f7f761bdb293b0
onurtaskirancom <taskiranonur9@gmail.com>
2022-04-06 19:23:00 +0300

    Show blog categories and tags

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -3,9 +3,7 @@ import Link from "next/link";
 import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
-import { API } from "../../config";
-import renderHtml from "react-render-html";
-import moment from "moment";
+import Card from '../../components/blog/Card';
 
 const Blogs = ({ blogs, categories, tags, size }) => {
   const showAllBlogs = () => {
@@ -13,36 +11,7 @@ const Blogs = ({ blogs, categories, tags, size }) => {
       // ()
       return (
         <article key={i}>
-          <div className='lead pb-4'>
-            <header>
-              <Link href={`/blogs/${blog.slug}`}>
-                <a>
-                  <h2 className='pt-3 pb-3 font-weight-bold'>{blog.title}</h2>
-                </a>
-              </Link>
-            </header>
-            <section>
-              <p className='mark ml-1 pt-2 pb-2'>
-                Written by {blog.postedBy.name} | Published{" "}
-                {moment(blog.updatedAt).fromNow()}
-              </p>
-            </section>
-            <section>
-              <p>blog categories and tags</p>
-            </section>
-
-            <div className='row'>
-              <div className='col-md-4'>image </div>
-              <div className='col-md-8'>
-                <section>
-                  <div className='pb-3'>{renderHtml(blog.excerpt)}</div>
-                  <Link href={`/blogs/${blog.slug}`}>
-                    <a className='btn btn-primary pt-2'>Read more</a>
-                  </Link>
-                </section>
-              </div>
-            </div>
-          </div>
+          <Card blog={blog} />
           <br />
         </article>
       );
