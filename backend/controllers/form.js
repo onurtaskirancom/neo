72f4c876f9d5924df37ad6a8111c233b1fa97294
onurtaskirancom <taskiranonur9@gmail.com>
2022-04-11 18:16:18 +0300

    Backend Contact blog author

--- backend/controllers/form.js
+++ backend/controllers/form.js
@@ -1,4 +1,4 @@
-const sgMail = require("@sendgrid/mail"); // SENDGRID_API_KEY
+const sgMail = require('@sendgrid/mail'); // SENDGRID_API_KEY
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.contactForm = (req, res) => {
@@ -17,7 +17,36 @@ exports.contactForm = (req, res) => {
             <p>Sender message: ${message}</p>
             <hr />
             <p>This email may contain sensetive information</p>
-            <p>https://neoblog.com</p>
+            <p>https://seoblog.com</p>
+        `
+    };
+
+    sgMail.send(emailData).then(sent => {
+        return res.json({
+            success: true
+        });
+    });
+};
+
+exports.contactBlogAuthorForm = (req, res) => {
+    const { authorEmail, email, name, message } = req.body;
+    // console.log(req.body);
+
+    let maillist = [authorEmail, process.env.EMAIL_TO];
+
+    const emailData = {
+        to: maillist,
+        from: email,
+        subject: `Someone messaged you from ${process.env.APP_NAME}`,
+        text: `Email received from contact from \n Sender name: ${name} \n Sender email: ${email} \n Sender message: ${message}`,
+        html: `
+            <h4>Message received from:</h4>
+            <p>name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Message: ${message}</p>
+            <hr />
+            <p>This email may contain sensetive information</p>
+            <p>https://seoblog.com</p>
         `
     };
 
